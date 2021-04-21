a1be8780e1620c01c54f21b781d794ac9d703633
Jacob Schenkelberg <jtschenk@ksu.edu>
2021-04-21 15:49:09 -0500

    take_sticks completed

--- Schenkelberg_Jacob_Homework4_Pictsie_Sticks.py
+++ Schenkelberg_Jacob_Homework4_Pictsie_Sticks.py
@@ -1,6 +1,7 @@
 from random import *
 
-num_sticks = 14 #Start with 20 sticks
+num_sticks = 20 #Start with 20 sticks
+current_player = 2
 
 def not_quite_right(num_sticks):
     if((0 <= num_sticks and num_sticks < 20)):
@@ -11,7 +12,8 @@ def not_quite_right(num_sticks):
             num_sticks = new_sticks + num_sticks
             print("{} stick(s) added. {} sticks remain.".format(new_sticks, num_sticks)) 
         else:
-            print("No sticks added. {} sticks remain.".format(num_sticks))      
+            print("No sticks added. {} sticks remain.".format(num_sticks))
+    return num_sticks      
 
 
 #find out a way to keep the line going
@@ -21,11 +23,18 @@ def display_board(num_sticks):
     for x in range(1, num_sticks+1):
         print(x, end= " ")
 
-not_quite_right(num_sticks)
-display_board(num_sticks)
 
-#def take_sticks(current_player, num_sticks):
+def take_sticks(current_player, num_sticks):
+    remove_sticks = 5 # set so the while loop works.
+    while(remove_sticks > 3):
+        print("Input a valid number to remove sticks. (1) (2) (3)")
+        remove_sticks = int(input("\nPlayer {}, how many sticks do you pickup? ".format(current_player)))
+    num_sticks = num_sticks - remove_sticks
+    return num_sticks
 
+#not_quite_right(num_sticks)
+#display_board(num_sticks)
+take_sticks(current_player, num_sticks)
 #def display_summary(player_number, sticks_taken, sticks_added, num_sticks):
 
 #def main():
