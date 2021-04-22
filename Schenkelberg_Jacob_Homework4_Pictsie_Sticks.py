cea3242f7173d03879f9becb46d984886c1f13af
Jacob Schenkelberg <jtschenk@ksu.edu>
2021-04-22 10:17:09 -0500

    some progress

--- Schenkelberg_Jacob_Homework4_Pictsie_Sticks.py
+++ Schenkelberg_Jacob_Homework4_Pictsie_Sticks.py
@@ -1,5 +1,8 @@
 from random import *
 
+global start
+start = True
+
 def not_quite_right(num_sticks):
     if((0 <= num_sticks and num_sticks < 20)):
         random_num = randint(1,10)
@@ -13,7 +16,6 @@ def not_quite_right(num_sticks):
     return num_sticks      
 
 
-#find out a way to keep the line going
 def display_board(num_sticks):
     for x in range(1, num_sticks+1):
         if(x < 10):
@@ -44,11 +46,7 @@ def display_board(num_sticks):
 
 
 def take_sticks(current_player, num_sticks):
-    current_player = 1
-    remove_sticks = 5 # set so the while loop works.
-    while(remove_sticks > 3):
-        print("Input a valid number to remove sticks. (1) (2) (3)")
-        remove_sticks = int(input("\nPlayer {}, how many sticks do you pickup? ".format(current_player)))
+    remove_sticks = int(input("\nPlayer {}, how many sticks do you pickup? ".format(current_player)))
     num_sticks = num_sticks - remove_sticks
     return num_sticks
 
@@ -56,10 +54,12 @@ def display_summary(current_player, remove_sticks, new_sticks, num_sticks):
     print("Player {} took {}, pictsie magic added {} back. There are {} sticks left".format(current_player, remove_sticks, new_sticks, num_sticks))
 
 def main():
-    num_sticks = 14 #Start with 20 sticks
-    current_player = 1
-    remove_sticks = 0
-    new_sticks = 0
+    if(start == True):
+        num_sticks = 15 #Start with 20 sticks
+        current_player = 1
+        remove_sticks = 0
+        new_sticks = 0
+        start == False
     while(num_sticks != 0):
         not_quite_right(num_sticks)
         display_board(num_sticks)
